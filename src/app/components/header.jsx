13e3952791a2f09d5383ff8733dc198133b64493
Sjaak Luthart <mail@sjaakluthart.nl>
2019-03-01 13:43:26 +0100

    Initial commit.

---
+++ src/app/components/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import beer from '../../assets/images/beer.svg';
+
+const Header = () => (
+  <header>
+    <img src={beer} alt="beer" />
+    <h1>Biertje</h1>
+  </header>
+);
+
+export default Header;
---
+++ src/app/index.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Header from './components/header';
+
+class App extends Component {
+  render() {
+    return (
+      <section className="main">
+        <Header />
+      </section>
+    );
+  }
+}
+
+export default App;
