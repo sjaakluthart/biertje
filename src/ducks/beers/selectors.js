fdfc2360e7436167618534088394ea80fb33d503
Sjaak Luthart <mail@sjaakluthart.nl>
2019-03-02 11:33:37 +0100

    Changed getBeersByStyle selector to allow selecting more than one style.

--- src/ducks/beers/selectors.js
+++ src/ducks/beers/selectors.js
@@ -1,11 +1,12 @@
 import get from 'lodash/get';
 import keys from 'lodash/keys';
+import pick from 'lodash/pick';
 
 const getBeers = state => state.byName;
 const getBeerNames = state => keys(state.byName);
 const getBeerByName = (state, name) => get(state.byName, name);
 const getStyles = state => keys(state.byStyle);
-const getBeersByStyle = (state, style) => get(state.byStyle, style);
+const getBeersByStyle = (state, styles) => pick(state.byStyle, styles);
 
 export {
   getBeers,
