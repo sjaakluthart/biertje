2b39b3de6ec89c02b5014ea2071c178dbb75f51e
Sjaak Luthart <mail@sjaakluthart.nl>
2019-03-02 11:44:02 +0100

    Added flatMap, renamed selector.

--- src/ducks/beers/selectors.js
+++ src/ducks/beers/selectors.js
@@ -1,17 +1,18 @@
 import get from 'lodash/get';
 import keys from 'lodash/keys';
 import pick from 'lodash/pick';
+import flatMap from 'lodash/flatMap';
 
 const getBeers = state => state.byName;
 const getBeerNames = state => keys(state.byName);
 const getBeerByName = (state, name) => get(state.byName, name);
 const getStyles = state => keys(state.byStyle);
-const getBeersByStyle = (state, styles) => pick(state.byStyle, styles);
+const getBeersNamesByStyle = (state, styles) => flatMap(pick(state.byStyle, styles));
 
 export {
   getBeers,
   getBeerNames,
   getBeerByName,
   getStyles,
-  getBeersByStyle
+  getBeersNamesByStyle
 };
