e1bb5ad328fc3d2b556805c449fac43ab86950bd
Sjaak Luthart <mail@sjaakluthart.nl>
2019-03-02 11:30:03 +0100

    Renamed selector.

--- src/ducks/beers/selectors.js
+++ src/ducks/beers/selectors.js
@@ -2,13 +2,13 @@ import get from 'lodash/get';
 import keys from 'lodash/keys';
 
 const getBeers = state => state.byName;
-const getBeer = (state, name) => get(state.byName, name);
+const getBeerByName = (state, name) => get(state.byName, name);
 const getStyles = state => keys(state.byStyle);
 const getBeersByStyle = (state, style) => get(state.byStyle, style);
 
 export {
   getBeers,
-  getBeer,
+  getBeerByName,
   getStyles,
   getBeersByStyle
 };
