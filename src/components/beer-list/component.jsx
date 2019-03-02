4df17a21be987b81987584ea7e79e244d57b43d9
Sjaak Luthart <mail@sjaakluthart.nl>
2019-03-02 11:46:53 +0100

    Added filter to BeerList.

--- src/components/beer-list/component.jsx
+++ src/components/beer-list/component.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import map from 'lodash/map';
+import isEmpty from 'lodash/isEmpty';
 import Beer from '../beer';
 
 const propTypes = {
-  beers: PropTypes.arrayOf(PropTypes.string).isRequired
+  beers: PropTypes.arrayOf(PropTypes.string).isRequired,
+  beersByStyle: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
 const BeerList = ({ beers, beersByStyle }) => {
+  if (isEmpty(beersByStyle)) {
+    return (
+      <section>
+        {map(beers, beer => <Beer key={beer} name={beer} />)}
+      </section>
+    );
+  }
+
   return (
     <section>
-      {map(beers, beer => <Beer key={beer} name={beer} />)}
+      {map(beersByStyle, beer => <Beer key={beer} name={beer} />)}
     </section>
   );
 };
--- src/components/beer-list/container.js
+++ src/components/beer-list/container.js
@@ -2,8 +2,9 @@ import { connect } from 'react-redux';
 import BeerList from './component';
 import { selectors as beersSelectors } from '../../ducks/beers';
 
-const mapStateToProps = state => ({
-  beers: beersSelectors.getBeerNames(state.beers)
+const mapStateToProps = (state, ownProps) => ({
+  beers: beersSelectors.getBeerNames(state.beers),
+  beersByStyle: beersSelectors.getBeersNamesByStyle(state.beers, ownProps.styles)
 });
 
 export default connect(mapStateToProps)(BeerList);
