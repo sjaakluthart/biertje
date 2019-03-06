0beac8da25c68bd62983f2402d26f2d588a3efd4
Sjaak Luthart <mail@sjaakluthart.nl>
2019-03-06 11:03:02 +0100

    Added Breweries to App.

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import Header from './components/header';
 import Beers from './components/beers';
+import Breweries from './components/breweries';
 
 class App extends Component {
+  state = {
+    activeTab: 'breweries'
+  }
+
   render() {
     return (
       <section className="main">
         <Header />
+        <Breweries />
         <Beers />
       </section>
     );
