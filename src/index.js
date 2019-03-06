c095d30c7faf635f8a9d05b026fe28fb7a6d208e
Sjaak Luthart <mail@sjaakluthart.nl>
2019-03-06 18:12:17 +0100

    Removed unused.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import 'whatwg-fetch';
 import App from './app';
 import store from './store';
 import './reset.css';
